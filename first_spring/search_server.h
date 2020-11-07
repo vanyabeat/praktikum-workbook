2d176628d03633c0423d1d70fa226d486d472bf7
vanyabeat <snelleswaine@ya.ru>
2020-11-07 14:52:54 +0300

    FIX: string functions

--- first_spring/search_server.h
+++ first_spring/search_server.h
@@ -86,14 +86,4 @@ private:
 
 	std::vector<Document> FindAllDocuments(const Query &query) const;
 
-	template<typename StringContainer>
-	static std::set<std::string> MakeUniqueNonEmptyStrings(const StringContainer &strings) {
-		std::set<std::string> non_empty_strings;
-		for (const std::string &str : strings) {
-			if (!str.empty()) {
-				non_empty_strings.insert(str);
-			}
-		}
-		return non_empty_strings;
-	}
 };
--- first_spring/string_processing.h
+++ first_spring/string_processing.h
@@ -1,5 +1,16 @@
 #pragma once
 #include <string>
 #include <vector>
-
+#include <set>
 std::vector<std::string> SplitIntoWords(const std::string &text);
+
+template<typename StringContainer>
+static std::set<std::string> MakeUniqueNonEmptyStrings(const StringContainer &strings) {
+	std::set<std::string> non_empty_strings;
+	for (const auto &str : strings) {
+		if (!str.empty()) {
+			non_empty_strings.insert(str);
+		}
+	}
+	return non_empty_strings;
+}

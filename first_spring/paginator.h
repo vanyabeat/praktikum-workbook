3e5e7c2d5d22122c8b9e411bf9b7898753ce66db
Sergei Chugainov <ru971a23@yandex.ru>
2020-11-10 01:14:59 +0500

    По файлам распределили правильно, но есть замечания: 1. старайтесь не допускать лишних копирования данных 2. ознакомтесь со структурами std::set и std::map а именно как их использовать для поиска 3. не забывайте защищать заголовочные файлы от повторного использования 4. предлагаю убрать реализации шаблонных и инлайновых методов из классов, это мешает обозревать класс целиком 5. старайтесь не использовать using в заголовочных файлах (можно внутри блоков и в cpp-файлах тоже нормально) 6. современные проекты можно реализовывать без "сырых" указателей, старайтесь их не использовать

--- first_spring/paginator.h
+++ first_spring/paginator.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <vector>
 #include <iterator>
-#include "document.h"
+#include "document.h"		/// не нашел использование структур из этого заголовочного файла
 
 template<typename It>
 class IteratorRange {
@@ -16,7 +16,7 @@ public:
 		return end_;
 	}
 
-	auto size() {
+	auto size() {		/// в данном месте лучше не использовать auto, если тип простой, то правильней его и использовать, метод не меняет поля, поэтому должен быть конкстантным
 		return size_;
 	}
 
@@ -30,13 +30,13 @@ template<typename Iterator>
 class Paginator {
 public:
 	explicit Paginator(Iterator b, Iterator e, size_t page_size) {
-		auto d = distance(b, e);
+		auto d = distance(b, e);						/// как это работает без "std::"? просто любопытно
 		// просто тупо будем определять кратно или не кратно)
 		size_ = d / page_size + (d % page_size != 0 ? 1 : 0);
 
 		for (int i = 0; i < size_; ++i) {
-			auto b_it = next(b, page_size * i);
-			auto e_it = (i == size_ - 1 ? e : next(b_it, page_size));
+			auto b_it = next(b, page_size * i);				/// как это работает без "std::"? просто любопытно
+			auto e_it = (i == size_ - 1 ? e : next(b_it, page_size));	/// как это работает без "std::"? просто любопытно
 			IteratorRange<Iterator> page(b_it, e_it, std::distance(b_it, e_it));
 			pages_.push_back(page);
 		}
@@ -50,13 +50,13 @@ public:
 		return pages_.end();
 	}
 
-	size_t size() {
-		return std::distance(pages_.begin(), pages_.end());
+	size_t size() {									/// метод не меняет поля класса, должен быть константным
+		return std::distance(pages_.begin(), pages_.end());			/// зачем каждый раз высчитывать? почему не использовать pages_.size() ?
 	}
 
 private:
 	std::vector<IteratorRange<Iterator>> pages_;
-	size_t size_;
+	size_t size_;									/// поле используется только как локальная пременная в конструкторе
 };
 
 
@@ -66,8 +66,8 @@ auto Paginate(const Container &c, size_t page_size) {
 }
 
 template<typename Iterator>
-std::ostream &operator<<(std::ostream &out, IteratorRange<Iterator> range) {
-	for (auto document : range) {
+std::ostream &operator<<(std::ostream &out, IteratorRange<Iterator> range) {		/// лишнее копирование range
+	for (auto document : range) {							/// лишнее копирование document
 		out << document;
 	}
 	return out;
--- first_spring/request_queue.h
+++ first_spring/request_queue.h
@@ -1,12 +1,12 @@
 #pragma once
-#include "search_server.h"
+#include "search_server.h"		/// в начале лучше поключать системные заголовочные файлы
 #include <deque>
 
 class RequestQueue {
 public:
 	explicit RequestQueue(const SearchServer &search_server);
 	// сделаем "обёртки" для всех методов поиска, чтобы сохранять результаты для нашей статистики
-	template<typename DocumentPredicate>
+	template<typename DocumentPredicate>	/// укажу только тут, но в остальных классах таже проблема, реализации шаблонных и инлайновых методов лучше убирать из класса, можно разместить под классом
 	std::vector<Document> AddFindRequest(const std::string &raw_query, DocumentPredicate document_predicate) {
 		auto request = search_server_.FindTopDocuments(raw_query, document_predicate);
 		QueryResult result(request);
@@ -28,7 +28,7 @@ private:
 
 	const static int sec_in_day_ = 1440;
 	// возможно, здесь вам понадобится что-то ещё
-	size_t count_empty_query;
+	size_t count_empty_query;	/// придерживайтесь порядка именования полей, у вас принято в классах в названии полей использовать символ подчеркивания в конце названия
 	const SearchServer &search_server_;
 
 	// метод добавления ответа сервера в очередь

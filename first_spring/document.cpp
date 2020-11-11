be662770eb71fd20e3af4c7a65e538e684d4684c
vanyabeat <snelleswaine@ya.ru>
2020-11-11 13:53:51 +0300

    FIX: last issues 11/11/2020 13:50

--- first_spring/document.cpp
+++ first_spring/document.cpp
@@ -1,6 +1,6 @@
 #include "document.h"
 
-std::ostream &operator<<(std::ostream &out, Document &document) {
+std::ostream &operator<<(std::ostream &out, const Document &document) {
 	out << "{ document_id = " << document.id << ", relevance = " << document.relevance << ", rating = " << document.rating << " }";
 	return out;
 }

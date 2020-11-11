f0d5afae68177dfb090265d792d6c57c42b89cc5
Sergei Chugainov <ru971a23@yandex.ru>
2020-11-11 12:08:37 +0500

    Уже лучше, пара небольших замечаний между ссылкой и константной есть разница использования, она существенная, и если ссылку можно сделать константной ее обязательно надо делать констатной, плюсов от этого будет больше

--- first_spring/paginator.h
+++ first_spring/paginator.h
@@ -65,7 +65,7 @@ auto Paginate(const Container &c, size_t page_size) {
 
 template<typename Iterator>
 std::ostream &operator<<(std::ostream &out, IteratorRange<Iterator> &range) {
-	for (auto &document : range) {
+	for (auto &document : range) {	/// ссылка должна быть константной
 		out << document;
 	}
 	return out;

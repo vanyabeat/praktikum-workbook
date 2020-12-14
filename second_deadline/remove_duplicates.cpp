163b05a1e2ea1a4648682d92ec8695c2772ba149
vanyabeat <vanyabeat@ya.ru>
2020-12-14 17:30:11 +0300

    FIX: 2.1 sprint issues 14.12.2020

--- second_deadline/remove_duplicates.cpp
+++ second_deadline/remove_duplicates.cpp
@@ -1,4 +1,5 @@
 #include "remove_duplicates.h"
+#include <set>
 
 /// вы воспользовались вектором как универсмальным контейнером, но другие типы контейнеров имеюют преимущества перед ним
 /// старайтесь после решения пересмотреть, может можно как то улучшить, и у вектора может быть достаточно дорогая вставка
@@ -6,17 +7,19 @@
 /// чем поиск в векторе
 /// слова тоже храните в векторе, вектор не гарантирует, порядок (он может быть различный) и уникальность, лучше тоже использховать другой контейнер
 /// в таком виде данная фукция не пройдет требования задачи по сложности, оне будет где-то O(N*N)
+
 void RemoveDuplicates(SearchServer &search_server) {
 
-	std::vector<std::vector<std::string>> all_docs;
-	std::vector<int> ids_to_remove;
+	std::set<std::set<std::string>> all_docs;
+
+	std::set<int> ids_to_remove;
 	for (const int document_id : search_server) {
 		auto doc = search_server.GetAllWordsInDocument(document_id);
 		if (std::find(all_docs.begin(), all_docs.end(), doc) == all_docs.end()) {
-			all_docs.push_back(doc);
+			all_docs.insert(doc);
 		} else {
 			std::cout << "Found duplicate document id " << document_id << std::endl;
-			ids_to_remove.push_back(document_id);
+			ids_to_remove.insert(document_id);
 		}
 	}
 	for (const auto id : ids_to_remove) {

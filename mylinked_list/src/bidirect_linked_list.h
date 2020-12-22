03e493ceaf7831a134dda04724ba44c09979506b
vanyabeat <vanyabeat@ya.ru>
2020-12-22 21:36:37 +0300

    ADD: add head and tail

--- mylinked_list/src/bidirect_linked_list.h
+++ mylinked_list/src/bidirect_linked_list.h
@@ -1,5 +1,7 @@
 #include <cstdio>
+#include <iostream>
 #include <iterator>
+#include <vector>
 
 template <typename Type> class BidirectionalList
 {
@@ -8,16 +10,21 @@ template <typename Type> class BidirectionalList
 	struct Node
 	{
 		Node() = default;
-		Node(const Type& val, Node* prev, Node* next) : value(val), prev_node(prev), next_node(next)
+		Node(Node* prev, const Type& val, Node* next) : prev_node(prev), value(val), next_node(next)
 		{
 		}
 		Type value;
 		Node* next_node = nullptr;
 		Node* prev_node = nullptr;
 	};
-#pragma region Constructors
-	BidirectionalList() : size_(0), head_(Node())
+#pragma region Constructors_and_Destructors
+	BidirectionalList() : size_(0), head_(Node()), tail_(Node())
 	{
+		head_.next_node = &tail_;
+		tail_.prev_node = &head_;
+	}
+	~BidirectionalList(){
+		clear();
 	}
 #pragma endregion
 #pragma region Iterator
@@ -26,16 +33,66 @@ template <typename Type> class BidirectionalList
 		using iterator_category = std::random_access_iterator_tag;
 		using difference_type = std::ptrdiff_t;
 		using value_type = ValueType;
-		using pointer = ValueType*;	// or also value_type*
+		using pointer = ValueType*;	  // or also value_type*
 		using reference = ValueType&; // or also value_type&
+
+		iterator(pointer ptr) : iterator_ptr_(ptr)
+		{
+		}
+		pointer iterator_ptr_;
 	};
 #pragma endregion
-	size_t size() const
+
+	// добавляет в начало O(1)
+	void push_front(const Type& value)
+	{
+		if (empty())
+		{
+			Node* tmp = new Node(&head_, value, &tail_);
+			head_.next_node = tmp;
+			tail_.prev_node = tmp;
+		}
+		else
+		{
+			head_.next_node = new Node(&head_, value, head_.next_node);
+		}
+		++size_;
+	}
+
+	// добавляет в конец O(1)
+	void push_back(const Type& value)
+	{
+		if (empty())
+		{
+			Node* tmp = new Node(&head_, value, &tail_);
+			head_.next_node = tmp;
+			tail_.prev_node = tmp;
+		}
+		else
+		{
+			tail_.prev_node = new Node(tail_.prev_node, value, &tail_);
+		}
+		++size_;
+	}
+
+	void clear() noexcept
+	{
+		while (head_.next_node != &tail_)
+		{
+			Node* next_element = head_.next_node;
+			head_.next_node = next_element->next_node;
+			delete next_element;
+		}
+
+		size_ = 0;
+	}
+
+	[[nodiscard]] size_t size() const noexcept
 	{
 		return size_;
 	}
 
-	bool empty() const
+	[[nodiscard]] bool empty() const noexcept
 	{
 		if (size_ == 0u)
 		{
@@ -47,4 +104,5 @@ template <typename Type> class BidirectionalList
   private:
 	size_t size_;
 	Node head_;
+	Node tail_;
 };
--- mylinked_list/tests/bidirect_linked_list_test.cpp
+++ mylinked_list/tests/bidirect_linked_list_test.cpp
@@ -16,7 +16,7 @@ class BidirectLinkedListTests : public ::testing::Test
 	}
 };
 
-TEST_F(BidirectLinkedListTests, Initialize)
+TEST_F(BidirectLinkedListTests, init)
 {
 	using namespace std;
 	{
@@ -29,5 +29,128 @@ TEST_F(BidirectLinkedListTests, Initialize)
 		const BidirectionalList<string> empty_string_list;
 		ASSERT_EQ(empty_string_list.size(), 0u);
 		ASSERT_TRUE(empty_string_list.empty());
+	}
+}
+
+TEST_F(BidirectLinkedListTests, push_front)
+{
+	using namespace std;
+	// Шпион, следящий за своим удалением
+
+	// Проверка вставки в начало
+	{
+		BidirectionalList<int> l;
+		ASSERT_TRUE(l.empty());
+		ASSERT_EQ(l.size(), 0u);
+
+		l.push_front(0);
+		l.push_front(1);
+		ASSERT_EQ(l.size(), 2);
+		ASSERT_TRUE(!l.empty());
+
+		l.clear();
+		ASSERT_EQ(l.size(), 0);
+		ASSERT_TRUE(l.empty());
+	}
+}
+
+TEST_F(BidirectLinkedListTests, PushBack)
+{
+	using namespace std;
+	// Шпион, следящий за своим удалением
+
+	// Проверка вставки в начало
+	{
+		BidirectionalList<int> l;
+		ASSERT_TRUE(l.empty());
+		ASSERT_EQ(l.size(), 0u);
+
+		l.push_back(1);
+		l.push_back(1);
+		l.push_back(3);
+		ASSERT_EQ(l.size(), 3);
+		ASSERT_TRUE(!l.empty());
+
+		l.clear();
+		ASSERT_EQ(l.size(), 0);
+		ASSERT_TRUE(l.empty());
+	}
+}
+
+TEST_F(BidirectLinkedListTests, DeletionSpy)
+{
+	struct DeletionSpy
+	{
+		DeletionSpy() = default;
+		explicit DeletionSpy(int& instance_counter) noexcept : instance_counter_ptr_(&instance_counter) //
+		{
+			OnAddInstance();
+		}
+		DeletionSpy(const DeletionSpy& other) noexcept : instance_counter_ptr_(other.instance_counter_ptr_) //
+		{
+			OnAddInstance();
+		}
+		DeletionSpy& operator=(const DeletionSpy& rhs) noexcept
+		{
+			if (this != &rhs)
+			{
+				auto rhs_copy(rhs);
+				std::swap(instance_counter_ptr_, rhs_copy.instance_counter_ptr_);
+			}
+			return *this;
+		}
+		~DeletionSpy()
+		{
+			OnDeleteInstance();
+		}
+
+	  private:
+		void OnAddInstance() noexcept
+		{
+			if (instance_counter_ptr_)
+			{
+				++(*instance_counter_ptr_);
+			}
+		}
+		void OnDeleteInstance() noexcept
+		{
+			if (instance_counter_ptr_)
+			{
+				ASSERT_NE(*instance_counter_ptr_, 0);
+				--(*instance_counter_ptr_);
+			}
+		}
+
+		int* instance_counter_ptr_ = nullptr;
+	};
+	// Проверка фактического удаления элементов
+	{
+		int item0_counter = 0;
+		int item1_counter = 0;
+		int item2_counter = 0;
+		{
+			BidirectionalList<DeletionSpy> list;
+			list.push_front(DeletionSpy{item0_counter});
+			list.push_front(DeletionSpy{item1_counter});
+			list.push_front(DeletionSpy{item2_counter});
+
+			ASSERT_EQ(item0_counter, 1);
+			ASSERT_EQ(item1_counter, 1);
+			ASSERT_EQ(item2_counter, 1);
+			list.clear();
+			ASSERT_EQ(item0_counter, 0);
+			ASSERT_EQ(item1_counter, 0);
+			ASSERT_EQ(item2_counter, 0);
+
+			list.push_front(DeletionSpy{item0_counter});
+			list.push_front(DeletionSpy{item1_counter});
+			list.push_front(DeletionSpy{item2_counter});
+			ASSERT_EQ(item0_counter, 1);
+			ASSERT_EQ(item1_counter, 1);
+			ASSERT_EQ(item2_counter, 1);
+		}
+		ASSERT_EQ(item0_counter, 0);
+		ASSERT_EQ(item1_counter, 0);
+		ASSERT_EQ(item2_counter, 0);
 	}
 }

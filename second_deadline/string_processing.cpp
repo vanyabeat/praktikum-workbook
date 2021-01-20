e36002e168d9965c2ea97d4c07f4cd42874589e1
vanyabeat <vanyabeat@ya.ru>
2021-01-20 14:14:17 +0300

    ADD: StringView parsing correct

--- second_deadline/string_processing.cpp
+++ second_deadline/string_processing.cpp
@@ -20,8 +20,9 @@ std::vector<std::string> SplitIntoWords(const std::string &text) {
 std::vector<std::string_view> SplitIntoWordsView(std::string_view str) {
 	std::vector<std::string_view> result;
 	const int64_t end = std::string_view::npos;
+	int64_t space;
 	while (true) {
-		int64_t space = str.find(' ');
+		space = str.find(' ');
 		result.push_back(str.substr(0, space));
 		if (space == end) {
 			break;

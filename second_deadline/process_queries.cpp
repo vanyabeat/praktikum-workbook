ec47b47076d6d03b02eb04c994ca88fcf8dd0762
vanyabeat <vanyabeat@ya.ru>
2021-01-25 15:37:17 +0300

    ADD: ProcessQueriesJoined

--- second_deadline/process_queries.cpp
+++ second_deadline/process_queries.cpp
@@ -10,3 +10,12 @@ std::vector<std::vector<Document>> ProcessQueries(const SearchServer& search_ser
 				   [&search_server](const std::string query) { return search_server.FindTopDocuments(query); });
 	return result;
 }
+std::vector<Document> ProcessQueriesJoined(const SearchServer& search_server, const std::vector<std::string>& queries)
+{
+	std::vector<Document> result;
+	for (const auto& local_documents : ProcessQueries(search_server, queries))
+	{
+		result.insert(result.end(), local_documents.begin(), local_documents.end());
+	}
+	return result;
+}

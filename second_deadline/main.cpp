f8706d1743adf0c11a195eb43003ce333cba067a
vanyabeat <vanyabeat@ya.ru>
2021-01-28 16:32:05 +0300

    ADD: execution::par

--- second_deadline/main.cpp
+++ second_deadline/main.cpp
@@ -575,6 +575,64 @@ void JoinAndReduce()
 		cout << "Document "s << document.id << " matched with relevance "s << document.relevance << endl;
 	}
 }
+
+void RemoveExecutionPolicy()
+{
+	using namespace std;
+	SearchServer search_server("and with"s);
+
+	int id = 0;
+	for (const string& text : {
+			 "funny pet and nasty rat"s,
+			 "funny pet with curly hair"s,
+			 "funny pet and not very nasty rat"s,
+			 "pet with rat and rat and rat"s,
+			 "nasty rat with curly hair"s,
+		 })
+	{
+		search_server.AddDocument(++id, text, DocumentStatus::ACTUAL, {1, 2});
+	}
+
+	const vector<string> queries = {"nasty rat -not"s, "not very funny nasty pet"s, "curly hair"s};
+	search_server.RemoveDocument(std::execution::seq, 1);
+}
+void ParralelMatch()
+{
+	using namespace std;
+	SearchServer search_server("and with"s);
+
+	int id = 0;
+	for (const string& text : {
+			 "funny pet and nasty rat"s,
+			 "funny pet with curly hair"s,
+			 "funny pet and not very nasty rat"s,
+			 "pet with rat and rat and rat"s,
+			 "nasty rat with curly hair"s,
+		 })
+	{
+		search_server.AddDocument(++id, text, DocumentStatus::ACTUAL, {1, 2});
+	}
+
+	const string query = "curly and funny -not"s;
+
+	{
+		const auto [words, status] = search_server.MatchDocument(query, 1);
+		cout << words.size() << " words for document 1"s << endl;
+		// 1 words for document 1
+	}
+
+	{
+		const auto [words, status] = search_server.MatchDocument(execution::seq, query, 2);
+		cout << words.size() << " words for document 2"s << endl;
+		// 2 words for document 2
+	}
+
+	{
+		const auto [words, status] = search_server.MatchDocument(execution::par, query, 3);
+		cout << words.size() << " words for document 3"s << endl;
+		// 0 words for document 3
+	}
+}
 // Функция TestSearchServer является точкой входа для запуска тестов
 void TestSearchServer()
 {
@@ -624,8 +682,12 @@ void TestSearchServer()
 	RUN_TEST(DuplicatesTest);
 	//	// 23
 	RUN_TEST(ReduceTest);
-
+	// 24
 	RUN_TEST(JoinAndReduce);
+	// 25
+	RUN_TEST(RemoveExecutionPolicy);
+	//26
+	RUN_TEST(ParralelMatch);
 }
 
 int main()

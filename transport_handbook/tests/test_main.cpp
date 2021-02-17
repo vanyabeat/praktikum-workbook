3f2d734ffa704f0da6822e23a8bfb690cb096fff
vanyabeat <snelleswaine@ya.ru>
2021-02-17 15:46:11 +0300

    ADD: parse (Bus X: ..) & (Stop X: latitude, longitude)

--- transport_handbook/tests/test_main.cpp
+++ transport_handbook/tests/test_main.cpp
@@ -14,8 +14,8 @@ TEST(Stop, Test) {
     ASSERT_EQ(a->getRequestType(), RequestType::IsStop);
     ASSERT_EQ(a->getName(), "Tolstopaltsevo"s);
 
-    ASSERT_DOUBLE_EQ(static_cast<Stop*>(a)->coordinates.lat, 55.611087);
-    ASSERT_DOUBLE_EQ(static_cast<Stop*>(a)->coordinates.lng, 37.208290);
+    ASSERT_DOUBLE_EQ(static_cast<Stop *>(a)->coordinates.lat, 55.611087);
+    ASSERT_DOUBLE_EQ(static_cast<Stop *>(a)->coordinates.lng, 37.208290);
     delete a;
 }
 
@@ -24,10 +24,27 @@ TEST(Bus, Test) {
     std::string parsable_string = "Bus 256: Biryulyovo Zapadnoye > Biryusinka > Universam > Biryulyovo Tovarnaya > Biryulyovo Passazhirskaya > Biryulyovo Zapadnoye"s;
 
     Request *a = ParseRequestString(parsable_string);
-    ASSERT_EQ(a->getRequestType(), RequestType::IsStop);
-    ASSERT_EQ(a->getName(), "Tolstopaltsevo"s);
+    ASSERT_EQ(a->getRequestType(), RequestType::IsBus);
+    ASSERT_EQ(a->getName(), "256"s);
+    auto stops = static_cast<Bus *>(a)->getStops();
+    ASSERT_EQ(stops, (std::vector<std::string>{
+            "Biryulyovo Zapadnoye", "Biryusinka", "Universam", "Biryulyovo Tovarnaya", "Biryulyovo Passazhirskaya",
+            "Biryulyovo Zapadnoye"
+    })
+    );
+    delete a;
+}
 
-    ASSERT_DOUBLE_EQ(static_cast<Stop*>(a)->coordinates.lat, 55.611087);
-    ASSERT_DOUBLE_EQ(static_cast<Stop*>(a)->coordinates.lng, 37.208290);
+TEST(Bus, Test2) {
+    using namespace std;
+    std::string parsable_string = "Bus 750: Tolstopaltsevo - Marushkino - Rasskazovka"s;
+
+    Request *a = ParseRequestString(parsable_string);
+    ASSERT_EQ(a->getRequestType(), RequestType::IsBus);
+    ASSERT_EQ(a->getName(), "750"s);
+    auto stops = static_cast<Bus *>(a)->getStops();
+    auto expected = std::vector<std::string>{"Tolstopaltsevo", "Marushkino", "Rasskazovka", "Marushkino",
+                                             "Tolstopaltsevo"};
+    ASSERT_EQ(stops, expected);
     delete a;
 }

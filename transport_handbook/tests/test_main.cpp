49b012138ad25926b70ab22d76ed300e7507ba4b
vanyabeat <vanyabeat@ya.ru>
2021-03-08 23:45:15 +0300

    ADD: trainer tasks (Shapes, Drawable.. etc)

--- transport_handbook/tests/test_main.cpp
+++ transport_handbook/tests/test_main.cpp
@@ -296,9 +296,8 @@ void DrawPicture()
 	doc.Render(std::cout);
 }
 
-
-
-TEST(SVG, Document2) {
+TEST(SVG, Document2)
+{
 	svg::Document doc;
 	doc.Add(svg::Circle().SetCenter({20, 20}).SetRadius(10));
 	doc.Add(svg::Text()
@@ -315,4 +314,41 @@ TEST(SVG, Document2) {
 TEST(SVG, Document)
 {
 	DrawPicture();
+}
+template <typename DrawableIterator>
+void DrawPicture(DrawableIterator begin, DrawableIterator end, svg::ObjectContainer& target)
+{
+	for (auto it = begin; it != end; ++it)
+	{
+		(*it)->Draw(target);
+	}
+}
+
+template <typename Container> void DrawPicture(const Container& container, svg::ObjectContainer& target)
+{
+	using namespace std;
+	DrawPicture(begin(container), end(container), target);
+}
+
+TEST(SVG, Shapes)
+{
+	using namespace svg;
+	using namespace shapes;
+	using namespace std;
+
+	vector<unique_ptr<svg::Drawable>> picture;
+
+	picture.emplace_back(make_unique<Triangle>(Point{100, 20}, Point{120, 50}, Point{80, 40}));
+	// 5-лучевая звезда с центром {50, 20}, длиной лучей 10 и внутренним радиусом 4
+	picture.emplace_back(make_unique<Star>(Point{50.0, 20.0}, 10.0, 4.0, 5));
+	// Снеговик с "головой" радиусом 10, имеющей центр в точке {30, 20}
+	picture.emplace_back(make_unique<Snowman>(Point{30, 20}, 10.0));
+
+	svg::Document doc;
+	// Так как документ реализует интерфейс ObjectContainer,
+	// его можно передать в DrawPicture в качестве цели для рисования
+	DrawPicture(picture, doc);
+
+	// Выводим полученный документ в stdout
+	doc.Render(cout);
 }

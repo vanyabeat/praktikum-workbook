aecf87160347fd2ad3b85ce3af2517f3478ec4b1
vanyabeat <vanyabeat@ya.ru>
2021-04-08 17:26:36 +0300

    BudgetManager 3/3

---
+++ budget_manager/bulk_updater.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "entities.h"
+#include "summing_segment_tree.h"
+
+#include <cstdint>
+#include <cmath>
+
+struct BulkMoneyAdder {
+    double delta = {};
+};
+
+struct BulkMoneySpender {
+    double delta = {};
+};
+
+struct BulkTaxApplier {
+    double proc_ = 1.0;
+};
+
+class BulkLinearUpdater {
+public:
+    BulkLinearUpdater() = default;
+
+    BulkLinearUpdater(const BulkMoneyAdder& add)
+        : add_(add) {
+    }
+
+    BulkLinearUpdater(const BulkMoneySpender& spnd)
+        : spnd_(spnd) {
+    }
+
+    BulkLinearUpdater(const BulkTaxApplier& tax)
+        : tax_(tax) {
+    }
+
+    void CombineWith(const BulkLinearUpdater& other) {
+        add_.delta = (add_.delta * other.tax_.proc_ ) + other.add_.delta;
+        spnd_.delta = spnd_.delta + other.spnd_.delta;
+        tax_.proc_ *= other.tax_.proc_;
+    }
+
+    Money Collapse(Money origin, IndexSegment segment) const {
+        Money res;
+        res.earn_ = (origin.earn_ * tax_.proc_) + add_.delta * static_cast<double>(segment.length());
+        res.spend_ = origin.spend_ + spnd_.delta * static_cast<double>(segment.length());
+        return res;
+    }
+
+private:
+    // apply tax first, then add
+    BulkTaxApplier tax_;
+    BulkMoneyAdder add_;
+    BulkMoneySpender spnd_;
+};

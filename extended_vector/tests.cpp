0fcad967df4bfee16e89e4861433b00f2c0d7f5d
vanyabeat <vanyabeat@ya.ru>
2021-05-05 10:52:55 +0300

    ADD: move operators in vector

--- extended_vector/tests.cpp
+++ extended_vector/tests.cpp
@@ -420,3 +420,144 @@ TEST(Vector, Test3_1)
 		ASSERT_EQ(Obj3::GetAliveObjectCount(), SIZE);
 	}
 }
+
+struct Obj4
+{
+	Obj4()
+	{
+		if (default_construction_throw_countdown > 0)
+		{
+			if (--default_construction_throw_countdown == 0)
+			{
+				throw std::runtime_error("Oops");
+			}
+		}
+		++num_default_constructed;
+	}
+
+	explicit Obj4(int id) : id(id) //
+	{
+		++num_constructed_with_id;
+	}
+
+	Obj4(const Obj4& other) : id(other.id) //
+	{
+		if (other.throw_on_copy)
+		{
+			throw std::runtime_error("Oops");
+		}
+		++num_copied;
+	}
+
+	Obj4(Obj4&& other) noexcept : id(other.id) //
+	{
+		++num_moved;
+	}
+
+	Obj4& operator=(const Obj4& other) = default;
+	Obj4& operator=(Obj4&& other) = default;
+
+	~Obj4()
+	{
+		++num_destroyed;
+		id = 0;
+	}
+
+	static int GetAliveObjectCount()
+	{
+		return num_default_constructed + num_copied + num_moved + num_constructed_with_id - num_destroyed;
+	}
+
+	static void ResetCounters()
+	{
+		default_construction_throw_countdown = 0;
+		num_default_constructed = 0;
+		num_copied = 0;
+		num_moved = 0;
+		num_destroyed = 0;
+		num_constructed_with_id = 0;
+	}
+
+	bool throw_on_copy = false;
+	int id = 0;
+
+	static inline int default_construction_throw_countdown = 0;
+	static inline int num_default_constructed = 0;
+	static inline int num_constructed_with_id = 0;
+	static inline int num_copied = 0;
+	static inline int num_moved = 0;
+	static inline int num_destroyed = 0;
+};
+
+TEST(Vector, Test4)
+{
+	const size_t MEDIUM_SIZE = 100;
+	const size_t LARGE_SIZE = 250;
+	const int ID = 42;
+	{
+		Obj4::ResetCounters();
+		Vector<int> v(MEDIUM_SIZE);
+		{
+			auto v_copy(std::move(v));
+
+			ASSERT_EQ(v_copy.Size(), MEDIUM_SIZE);
+			ASSERT_EQ(v_copy.Capacity(), MEDIUM_SIZE);
+		}
+		ASSERT_EQ(Obj4::GetAliveObjectCount() , 0);
+	}
+	{
+		Obj4::ResetCounters();
+		{
+			Vector<Obj4> v(MEDIUM_SIZE);
+			v[MEDIUM_SIZE / 2].id = ID;
+			ASSERT_EQ(Obj4::num_default_constructed, MEDIUM_SIZE);
+			Vector<Obj4> moved_from_v(std::move(v));
+			ASSERT_EQ(moved_from_v.Size(), MEDIUM_SIZE);
+			ASSERT_EQ(moved_from_v[MEDIUM_SIZE / 2].id, ID);
+		}
+		ASSERT_EQ(Obj4::GetAliveObjectCount(), 0);
+
+		ASSERT_EQ(Obj4::num_moved, 0);
+		ASSERT_EQ(Obj4::num_copied, 0);
+		ASSERT_EQ(Obj4::num_default_constructed, MEDIUM_SIZE);
+	}
+	{
+		Obj4::ResetCounters();
+		Vector<Obj4> v_medium(MEDIUM_SIZE);
+		v_medium[MEDIUM_SIZE / 2].id = ID;
+		Vector<Obj4> v_large(LARGE_SIZE);
+		v_large = v_medium;
+		ASSERT_EQ(v_large.Size(), MEDIUM_SIZE);
+		ASSERT_EQ(v_large.Capacity(), LARGE_SIZE);
+		ASSERT_EQ(v_large[MEDIUM_SIZE / 2].id, ID);
+		ASSERT_EQ(Obj4::GetAliveObjectCount(), MEDIUM_SIZE + MEDIUM_SIZE);
+	}
+	{
+		Obj4::ResetCounters();
+		Vector<Obj4> v(MEDIUM_SIZE);
+		{
+			Vector<Obj4> v_large(LARGE_SIZE);
+			v_large[LARGE_SIZE - 1].id = ID;
+			v = v_large;
+			ASSERT_EQ(v.Size(), LARGE_SIZE);
+			ASSERT_EQ(v_large.Capacity(), LARGE_SIZE);
+			ASSERT_EQ(v_large[LARGE_SIZE - 1].id, ID);
+			ASSERT_EQ(Obj4::GetAliveObjectCount(), LARGE_SIZE + LARGE_SIZE);
+		}
+		ASSERT_EQ(Obj4::GetAliveObjectCount() , LARGE_SIZE);
+	}
+	assert(Obj4::GetAliveObjectCount() == 0);
+	{
+		Obj4::ResetCounters();
+		Vector<Obj4> v(MEDIUM_SIZE);
+		v[MEDIUM_SIZE - 1].id = ID;
+		Vector<Obj4> v_small(MEDIUM_SIZE / 2);
+		v_small.Reserve(MEDIUM_SIZE + 1);
+		const size_t num_copies = Obj4::num_copied;
+		v_small = v;
+		ASSERT_EQ(v_small.Size() , v.Size());
+		ASSERT_EQ(v_small.Capacity() , MEDIUM_SIZE + 1);
+		v_small[MEDIUM_SIZE - 1].id = ID;
+		ASSERT_EQ(Obj4::num_copied - num_copies , MEDIUM_SIZE - (MEDIUM_SIZE / 2));
+	}
+}
--- extended_vector/vector.h
+++ extended_vector/vector.h
@@ -1,11 +1,11 @@
 #pragma once
+#include "raw_memory.h"
+#include <algorithm>
 #include <cassert>
 #include <cstdlib>
 #include <memory>
 #include <new>
 #include <utility>
-
-#include "raw_memory.h"
 
 template <typename T> class Vector
 {
@@ -20,6 +20,61 @@ template <typename T> class Vector
 	Vector(const Vector& other) : data_(other.size_), capacity_(other.size_), size_(other.size_) //
 	{
 		std::uninitialized_copy_n(other.data_.GetAddress(), other.size_, data_.GetAddress());
+	}
+
+	Vector(Vector&& other_) noexcept
+		: data_(std::move(other_.data_)), capacity_(std::move(other_.capacity_)), size_(std::move(other_.size_))
+	{
+		other_.capacity_ = 0;
+		other_.size_ = 0;
+	}
+
+	Vector& operator=(const Vector& right_)
+	{
+		if (this != &right_)
+		{
+			if (right_.size_ > data_.Capacity())
+			{
+				/* Применить copy-and-swap */
+				Vector copy(right_);
+				Swap(copy);
+			}
+			else
+			{
+				if (!(size_ < right_.size_))
+				{
+					std::copy_n(right_.data_.GetAddress(), right_.size_, data_.GetAddress());
+					std::destroy_n(data_.GetAddress() + right_.size_, size_ - right_.size_);
+					size_ = right_.size_;
+				}
+				else
+				{
+					;
+					for (auto [it_r, it_this] = std::tuple{right_.data_.GetAddress(), data_.GetAddress()};
+						 it_r != right_.data_.GetAddress() + size_; ++it_r, ++it_this)
+					{
+						*it_this = *it_r;
+					}
+					std::uninitialized_copy_n(right_.data_.GetAddress() + size_, right_.size_ - size_,
+											  data_.GetAddress() + size_);
+					size_ = right_.size_;
+				}
+			}
+		}
+		return *this;
+	}
+
+	Vector& operator=(Vector&& right_) noexcept
+	{
+		if (this != &right_)
+		{
+			data_ = std::move(right_.data_);
+			capacity_ = std::move(right_.capacity_);
+			size_ = std::move(right_.size_);
+			right_.capacity_ = 0;
+			right_.size_ = 0;
+		}
+		return *this;
 	}
 
 	void Reserve(size_t new_capacity)
@@ -70,6 +125,20 @@ template <typename T> class Vector
 	{
 		assert(index < size_);
 		return data_[index];
+	}
+
+	void Swap(Vector& other) noexcept
+	{
+		using std::swap;
+		swap(*this, other);
+	}
+
+	friend void swap(Vector<T>& first, Vector<T>& second) noexcept
+	{
+		using std::swap;
+		swap(first.data_, second.data_);
+		swap(first.capacity_, second.capacity_);
+		swap(first.size_, second.size_);
 	}
 
   private:
